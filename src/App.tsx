67f1d59feee3a1c9017af2346c8acea71dc65d8c
Fernando chicaiza <washopilot@yahoo.com>
2022-05-03 11:22:27 -0500

    implementando un calendario

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import '@progress/kendo-theme-default/dist/all.css';
 import './App.css';
+import { Calendar } from '@progress/kendo-react-dateinputs';
 
 class App extends Component {
-  render(): JSX.Element {
+  render() {
     return (
       <div className="App">
         <h1>Hello KendoReact!</h1>
+        <Calendar />
       </div>
     );
   }
